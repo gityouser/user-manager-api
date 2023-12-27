dc50d853e251d01f2ce24ff6a2394adcbf645f50
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2023-12-27 19:53:29 +0200

    scaffold users modules and test findAll

---
+++ src/users/users.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private users: any[] = [
+    {
+      id: 1,
+      name: 'John Doe',
+      roles: ['ADMIN', 'PERSONAL'],
+      groups: ['GROUP_1', 'GROUP_2'],
+    },
+
+    {
+      id: 2,
+      name: 'Grabriel Monroe',
+      roles: ['PERSONAL'],
+      groups: ['GROUP_1', 'GROUP_2'],
+    },
+
+    { id: 3, name: 'Alex Xavier', roles: ['PERSONAL'], groups: ['GROUP_2'] },
+
+    {
+      id: 4,
+      name: 'Jarvis Khan',
+      roles: ['ADMIN', 'PERSONAL'],
+      groups: ['GROUP_2'],
+    },
+
+    {
+      id: 5,
+      name: 'Martines Polok',
+      roles: ['ADMIN', 'PERSONAL'],
+      groups: ['GROUP_1'],
+    },
+
+    {
+      id: 6,
+      name: 'Gabriela Wozniak',
+      roles: ['VIEWER', 'PERSONAL'],
+      groups: ['GROUP_1'],
+    },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+}
