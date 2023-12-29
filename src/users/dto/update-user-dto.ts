843a9945b54edbd82dcb184547efaa97337b1648
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2023-12-29 14:07:24 +0200

    add swagger doc builder and api decorators for controller and dtos

--- src/users/dto/update-user-dto.ts
+++ src/users/dto/update-user-dto.ts
@@ -10,22 +10,34 @@ import {
 import { GroupValue, RoleValue } from '../types/user.types';
 import { Role } from '../enum/role.enum';
 import { Group } from '../enum/group.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
   @IsOptional()
   @IsString()
   @MaxLength(100)
   @MinLength(3) // Added on top of the specs
+  @ApiProperty({ description: 'Name of the User', required: false })
   name?: string;
 
   @IsOptional()
   @ArrayMinSize(1)
   @IsEnum(Role, { each: true })
+  @ApiProperty({
+    description: 'Roles of the User',
+    type: [String],
+    required: false,
+  })
   roles?: RoleValue[];
 
   @IsOptional()
   @IsArray()
   @ArrayMinSize(1)
   @IsEnum(Group, { each: true })
+  @ApiProperty({
+    description: 'Groups of the User',
+    type: [String],
+    required: false,
+  })
   groups?: GroupValue[];
 }
