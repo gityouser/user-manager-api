9c43c5cebf5e0e2d99f740eae5066321fc25c876
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2024-01-04 00:04:27 +0200

    inherit partial types and validations from create to update user dto

--- src/users/dto/update-user-dto.ts
+++ src/users/dto/update-user-dto.ts
@@ -1,31 +1,4 @@
-import {
-  IsString,
-  IsArray,
-  IsOptional,
-  MinLength,
-  MaxLength,
-  ArrayMinSize,
-  IsEnum,
-} from 'class-validator';
-import { GroupValue, RoleValue } from '../types/user.types';
-import { Role } from '../enum/role.enum';
-import { Group } from '../enum/group.enum';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto {
-  @IsOptional()
-  @IsString()
-  @MaxLength(100)
-  @MinLength(3) // Added on top of the specs
-  name?: string;
-
-  @IsOptional()
-  @ArrayMinSize(1)
-  @IsEnum(Role, { each: true })
-  roles?: RoleValue[];
-
-  @IsOptional()
-  @IsArray()
-  @ArrayMinSize(1)
-  @IsEnum(Group, { each: true })
-  groups?: GroupValue[];
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
